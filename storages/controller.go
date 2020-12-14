d2563febc5fa5e50c0b0d156417c284392f6c0e6
ali-furkqn <mail@alifurkan.codes>
2020-12-14 16:29:46 +0300

    🛠 Edit (storages): Added Controller Handler

--- storages/controller.go
+++ storages/controller.go
@@ -1,7 +1,18 @@
 package storages
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"stona/auth"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func (m *StorageModule) Init(router fiber.Router) {
+	sRouter := router.Group(m.Path)
+	sRouter.Get("/:path/:id", Service().GetAsset)
 
+	sRouter.Get("/:path", Service().GetList)
+
+	sRouter.Put("/:path/:id", auth.Service().AuthMiddleware, Service().Upload)
+	sRouter.Put("/:path", auth.Service().AuthMiddleware, Service().Upload)
+	sRouter.Delete("/:path/:id", auth.Service().AuthMiddleware, Service().Delete)
 }
