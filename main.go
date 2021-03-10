8d73350fad4fdd8dc82abd22961131e7d2644009
ali-furkqn <a.furkan.2003@gmail.com>
2021-03-10 20:13:55 +0300

    ♻️ (main): Adapted for new stona

--- main.go
+++ main.go
@@ -1,59 +1,11 @@
 package main
 
 import (
-	"log"
-	"strings"
-	"time"
+	"os"
 
-	"github.com/ali-furkqn/stona/auth"
-	"github.com/ali-furkqn/stona/config"
-	"github.com/ali-furkqn/stona/storages"
-
-	"github.com/fatih/color"
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/etag"
-	"github.com/gofiber/fiber/v2/middleware/limiter"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"github.com/ali-furkqn/stona/cmd"
 )
 
-var storage *storages.StorageModule
-
-func init() {
-	// Only one storage can add for now.
-	// In the future multiple storages can
-	// be created with configuration
-
-	storage = storages.NewStorage(config.Config.StoragePath, "firebase")
-
-}
-
 func main() {
-	app := fiber.New()
-
-	app.Use(etag.New())
-	app.Use(cors.New())
-	app.Use(requestid.New())
-	app.Use(limiter.New(limiter.Config{
-		Max:        24,
-		Expiration: 30 * time.Second,
-	}))
-
-	format := []string{color.HiBlackString("${time}"), color.BlueString("[Request]"), color.CyanString("${status} - ${method} ${path}"), color.YellowString("+${latency}\n")}
-	app.Use(logger.New(logger.Config{
-		Format: strings.Join(format, " "),
-	}))
-
-	mainRouter := app.Group(config.Config.RootPath)
-
-	auth.Init(mainRouter)
-
-	storage.Init(mainRouter)
-
-	PORT := config.GetPort()
-
-	if err := app.Listen(PORT); err != nil {
-		log.Fatalln(err)
-	}
+	os.Exit(cmd.Run(os.Args[1:]))
 }
